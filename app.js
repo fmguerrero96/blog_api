754fb6a06c6a6bb934ac66da8264b6cf874cb4fa
Misael Guerrero <mguerrerowest@gmail.com>
2023-12-17 16:35:09 -0600

    Connect to mongodb database with mongoose

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express()
 
 const PORT = (process.env.PORT || 2000);
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
