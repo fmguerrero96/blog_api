7f861c940461bd97753ca1877bdf796b4f901d8a
Misael Guerrero <mguerrerowest@gmail.com>
2023-12-20 17:24:11 -0600

    Add a 'comments' porperty to 'post' model

--- models/post.js
+++ models/post.js
@@ -5,7 +5,8 @@ const PostSchema = new Schema({
     title: {type: String, required: true, maxLength: 100},
     text: {type: String, required: true},
     time_stamp: { type: Date, default: Date.now },
-    public: {type: Boolean, required: true, default: true}
+    public: {type: Boolean, required: true, default: true},
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
 })
 
 module.exports = mongoose.model("Post", PostSchema);
