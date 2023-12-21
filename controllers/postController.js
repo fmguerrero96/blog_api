f01fd2077454dda92b6cf6ce0fd2b49f5d04fc22
Misael Guerrero <mguerrerowest@gmail.com>
2023-12-21 17:59:59 -0600

    Create controller function for handling creation of new blog post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,7 @@
 const Post = require("../models/post")
+const Comment = require("../models/comment")
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 //GET list of all posts
 exports.getAllPosts = asyncHandler(async (req, res, next) => {
@@ -20,4 +22,47 @@ exports.getSinglePost = asyncHandler(async (req, res, next) => {
       }
 
     res.json(singlePost)
+});
+
+//Create one blog post
+exports.createBlogPost = asyncHandler(async (req, res, next) => {
+    //Validate and sanitize data
+    body('title')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Please provide a title")
+    body('text')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Please provide the body for this blog post")
+    body('public')
+        .isBoolean()
+        .withMessage('Please specify if blog post is public or not')
+    
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()) {
+        //if there are erros, then...
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    //Create new blog post
+    const newPost = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        public: req.body.public,
+        comments: []
+    })
+
+    try {
+        // Save the new post to the database
+        const savedPost = await newPost.save();
+    
+        res.status(201).json(savedPost); // Respond with the created post
+      } catch (error) {
+        // Handle any errors that occur during the save operation
+        res.status(500).json({ error: 'Internal Server Error. Could not create blog post' });
+      }
 });
