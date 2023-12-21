23676cc625af0b4da6c89dbbbfc20bc44358c032
Misael Guerrero <mguerrerowest@gmail.com>
2023-12-21 16:24:46 -0600

    Create middleware function to reurn a specific post

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,3 +6,18 @@ exports.getAllPosts = asyncHandler(async (req, res, next) => {
     const allPosts = await Post.find({}).populate('comments').sort({ title: 1 }).exec();
     res.json(allPosts)
 });
+
+//Get a specific blog post
+exports.getSinglePost = asyncHandler(async (req, res, next) => {
+    const singlePost = await Post.findById(req.params.postid)
+    .populate('comments').exec()
+
+    if (singlePost === null) {
+        // No results.
+        const err = new Error("Post not found");
+        err.status = 404;
+        return next(err);
+      }
+
+    res.json(singlePost)
+});
