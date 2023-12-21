b79408b42806f55671809c598787f5c3c48ed479
Misael Guerrero <mguerrerowest@gmail.com>
2023-12-21 16:23:51 -0600

    Create route to get a specific blog post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,4 +8,7 @@ const posts_controller = require("../controllers/postController");
 //GET list of all posts 
 router.get('/posts', posts_controller.getAllPosts);
 
+//Get a specific blog post
+router.get("/posts/:postid", posts_controller.getSinglePost);
+
 module.exports = router;
