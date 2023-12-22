ca571bb90f66031e21c5a09a27741a7bd7aeef4f
Misael Guerrero <mguerrerowest@gmail.com>
2023-12-22 16:35:14 -0600

    Create function to handle blog post update

--- controllers/postController.js
+++ controllers/postController.js
@@ -74,10 +74,57 @@ exports.deleteBlogPost = asyncHandler(async (req, res, next) => {
 
     if(!post){
         //if post does not exist...
-        res.json('The blog post you are trying to delete does not exist')
+        res.json('The blog post you are trying to delete does not exist.')
     } else {
         //delete post if it exists
         await Post.findByIdAndDelete(req.params.postid)
         res.json(`Post with id ${req.params.postid} has been deleted.`)
+    }
+});
+
+//Update a blog post
+exports.updateBlogPost = asyncHandler(async (req, res, next) => {
+    //Validate and sanitize data
+    body('title')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Please provide a title")
+    body('text')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Please provide the body for this blog post")
+    body('public')
+        .isBoolean()
+        .withMessage('Please specify if blog post is public or not')
+    
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()) {
+        //if there are erros, then...
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+        // find the existing post
+        const existingPost = await Post.findById(req.params.postid);
+
+        if (!existingPost) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        // Update the post fields
+        existingPost.title = req.body.title;
+        existingPost.text = req.body.text;
+        existingPost.public = req.body.public;
+
+        // Save the updated post to the database
+        const updatedPost = await existingPost.save();
+
+        res.status(200).json(updatedPost); // Respond with the updated post
+    } catch (error) {
+        // Handle any errors that occur during the update operation
+        res.status(500).json({ error: 'Internal Server Error. Could not update the blog post' });
     }
 });
