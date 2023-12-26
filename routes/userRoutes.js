b021c4b78f3f2e5c32ca07dcc08e8bf4d747da80
Misael Guerrero <mguerrerowest@gmail.com>
2023-12-26 15:56:17 -0600

    Create endpoint for creating a new user on POST

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,4 +11,7 @@ router.get('/users', user_controller.getAllUsers);
 //Get a specific user by id
 router.get('/users/:userid', user_controller.getSingleUser);
 
+//Create a user
+router.post('/users', user_controller.createUser);
+
 module.exports = router;
