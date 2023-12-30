46cb47de4631c8f9f0ca10531fe90c9ca68fdd43
Misael Guerrero <mguerrerowest@gmail.com>
2023-12-30 16:06:29 -0600

    Create function to hendle fetching all the comments

---
+++ controllers/commentController.js
@@ -0,0 +1,21 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+const User = require('../models/user');
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+exports.getAllComments = asyncHandler(async (req, res, next) => {
+    try {
+        const allComments = await Comment.find({})
+            .populate({
+                path: 'author',
+                select: 'first_name username', 
+            })
+            .populate('post')
+            .sort({ 'author.first_name': 1 });
+        res.status(200).json(allComments);
+    } catch (error) {
+        // Handle database query error
+        res.status(500).json({ error: 'Internal Server Error. Could not fetch comments.' });
+    }
+});
