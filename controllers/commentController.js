97dd7ef4261c357c915d89615c33f6f83ab36379
Misael Guerrero <mguerrerowest@gmail.com>
2023-12-30 16:18:57 -0600

    Create function to fetch a single comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -21,3 +21,21 @@ exports.getAllComments = asyncHandler(async (req, res, next) => {
         res.status(500).json({ error: 'Internal Server Error. Could not fetch comments.' });
     }
 });
+
+
+//fetch a single comment
+exports.getSingleComment = asyncHandler(async (req, res, next) => {
+    try {
+        const singleComment = await Comment.findById(req.params.commentid)
+            .populate({
+                path: 'author',
+                select: 'first_name username', 
+            })
+            .populate('post')
+            .exec();
+        res.status(200).json(singleComment)
+    } catch (error) {
+        // Handle database query error
+        res.status(500).json({ error: 'Internal Server Error. Could not fetch the comment you are looking for.' });
+    }
+});
