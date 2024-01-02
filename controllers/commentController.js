25b41e4e5a55d84d462b13ea70c8de843622905f
Misael Guerrero <mguerrerowest@gmail.com>
2024-01-02 13:18:39 -0600

    Create function to handle creation of new comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -39,3 +39,51 @@ exports.getSingleComment = asyncHandler(async (req, res, next) => {
         res.status(500).json({ error: 'Internal Server Error. Could not fetch the comment you are looking for.' });
     }
 });
+
+//Create a new comment 
+exports.createComment = asyncHandler(async (req, res, next) => {
+    //Validate and sanitize the data
+    body('message')
+        .trim()
+        .isLength({ min: 1, max: 300 })
+        .escape()
+        .withMessage('Comment must have between 1 and 300 characters');
+    
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()) {
+        //if there are erros, then...
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+        // extract the user's id
+        const authorId = req.user._id;
+
+        // extract the postid
+        const postId = req.params.postid;
+
+        // Check if the post exists
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        // Create the new comment
+        const newComment = new Comment({
+            author: authorId,
+            post: postId,
+            message: req.body.message,
+            time_stamp: new Date(),
+        });
+
+        const savedComment = await newComment.save();
+
+        return res.status(201).json(savedComment);
+
+    } catch(error) {
+        // Handle any errors that occur during the save operation
+        res.status(500).json({ error: 'Internal Server Error. Could not create a new comment' });
+    }
+});
