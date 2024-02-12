84d2be81606436ead31b4fe3acf127660aa032dc
Misael Guerrero <mguerrerowest@gmail.com>
2024-02-12 10:02:11 -0500

    Create passport config file and set up jwt startegy

---
+++ passport-config.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcryptjs');
+const User = require('./models/user');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'this_is_the_secret_key',
+};
+
+passport.use(new JwtStrategy(options, async (jwtPayload, done) => {
+    try {
+        const user = User.findById(jwtPayload.id)
+
+        if (user) {
+            return done(null, user)
+        } else {
+            return done(null, false)
+        }
+    } catch (error) {
+        return done(error, false)
+    }
+}));
+
