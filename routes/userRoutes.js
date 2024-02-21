252cfa80c7e3208701be47450dac8d9f85209fb8
Misael Guerrero <mguerrerowest@gmail.com>
2024-02-21 17:09:12 -0500

    Create authentication middleware to protect routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+
+// authentication middleware function to protect routes
+const authenticateJWT = passport.authenticate('jwt', { session: false });
 
 const user_controller = require('../controllers/userController');
 
